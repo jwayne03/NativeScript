88ba1bc882f2836ca76d289310b1d2167f9b1709
jwayne03 <johnwayne.carreon@gmail.com>
2020-10-24 21:11:20 +0200

    #4 make change color

--- NativeScriptJohnWayne/src/app/practica1/practica1.component.ts
+++ NativeScriptJohnWayne/src/app/practica1/practica1.component.ts
@@ -3,9 +3,36 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'selector-name',
     templateUrl: "./practica1.component.html",
-    styleUrls : ["./practica1.component.css"]
+    styleUrls: ["./practica1.component.css"]
 })
 
 export class Practica1Component {
+    topblock = "topblock";
+    leftblock = "leftblock";
+    rightblock = "rightblock";
+    bottomblock = "bottomblock";
 
+    optionOne() {
+        this.topblock = "topblock";
+    }
+    // make change the color in color blue
+    optionTwo() {
+        this.topblock = "rightblock";
+    }
+
+    optionThree() {
+
+    }
+
+    optionFour() {
+
+    }
+
+    optionFive() {
+
+    }
+
+    optionSix() {
+
+    }
 }
