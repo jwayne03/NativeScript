ce3756bf941712292625a828bfb32290fb252338
Wayne <johnwayne.carreon@gmail.com>
2020-10-24 17:19:39 +0200

    #3 doing the part of layout

--- NativeScriptJohnWayne/src/app/practica1/practica1.component.ts
+++ NativeScriptJohnWayne/src/app/practica1/practica1.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'selector-name',
-    templateUrl: 'practica1.component.html'
+    templateUrl: "./practica1.component.html",
+    styleUrls : ["./practica1.component.css"]
 })
 
 export class Practica1Component {
